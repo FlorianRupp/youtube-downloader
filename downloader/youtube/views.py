881d43a379c15d8603ad6a8579c9d6a0f19ed2f9
florian.rupp95@gmail.com <florian.rupp95@gmail.com>
2023-01-31 13:53:33 +0100

    Integrated Discord message after download.

--- downloader/youtube/views.py
+++ downloader/youtube/views.py
@@ -1,16 +1,37 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
 # Create your views here.
 import subprocess
+import threading
+
+from discord import Webhook, RequestsWebhookAdapter
+from django.http import HttpResponse
+from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
 
 OUTPUT_DIR = "/home/ailab/karaoke/media"
 
 
+def popen_and_call(on_exit, popen_args, exit_args):
+    def run_in_thread(on_exit, popen_args):
+        proc = subprocess.Popen(popen_args)
+        proc.wait()
+        on_exit(*exit_args)
+        return
+
+    thread = threading.Thread(target=run_in_thread, args=(on_exit, popen_args))
+    thread.start()
+    # returns immediately after the thread starts
+    return thread
+
+
+def to_discord(interpret, song):
+    webhook = Webhook.from_url(
+        "https://discord.com/api/webhooks/1069963059428343918/heT2Qy7tXdw_w-RDZNwYH5ZzBaO6r5_q2wgiOMehnQKE8qCFVf_IX21KsUPirAjp_2Vg",
+        adapter=RequestsWebhookAdapter())
+    webhook.send(f"Downloaded Karaoke Song: {interpret} - {song}")
+
+
 @csrf_exempt
 def index(request):
-
     if request.POST:
         try:
             song = request.POST.get("song")
@@ -18,8 +39,13 @@ def index(request):
             link = request.POST.get("link")
             print(f"Download {song}")
 
-            subprocess.Popen(["youtube-dl", "-f", "best", "--output", f"{OUTPUT_DIR}/{interpret} - {song}.mp4", link])
-            return HttpResponse("Download started... Wait up to 3 min.")
+            # subprocess.Popen(["youtube-dl", "-f", "best", "--output", f"{OUTPUT_DIR}/{interpret} - {song}.mp4", link])
+            command = ["youtube-dl", "-f", "best", "--output", f"{OUTPUT_DIR}/{interpret} - {song}.mp4", link]
+            popen_and_call(on_exit=to_discord, popen_args=command, exit_args=[interpret, song])
+
+            template = loader.get_template('download_started.html')
+            return HttpResponse(template.render({}, request))
+
         except Exception:
             template = loader.get_template('index.html')
             return HttpResponse(template.render({}, request))
