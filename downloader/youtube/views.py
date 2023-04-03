43bd7bd08d27cff47904d324c0c28eb471785c8a
florian.rupp95@gmail.com <florian.rupp95@gmail.com>
2023-04-03 17:16:41 +0200

    Automatic page refresh after downloading song.

--- downloader/youtube/views.py
+++ downloader/youtube/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 import subprocess
 import threading
+import requests
 
 from discord import Webhook, RequestsWebhookAdapter
 from django.http import HttpResponse
@@ -11,11 +12,14 @@ from django.views.decorators.csrf import csrf_exempt
 OUTPUT_DIR = r"C:\Users\Florian Rupp\Desktop\Karaoke\media"
 
 
-def popen_and_call(on_exit, popen_args, exit_args):
+def popen_and_call(on_exit, popen_args, exit_args=None):
+    if exit_args is None:
+        exit_args = []
+
     def run_in_thread(on_exit, popen_args):
         proc = subprocess.Popen(popen_args)
-        # proc.wait()
-        # on_exit(*exit_args)
+        proc.wait()
+        on_exit(*exit_args)
         return
 
     thread = threading.Thread(target=run_in_thread, args=(on_exit, popen_args))
@@ -31,6 +35,13 @@ def to_discord(interpret, song):
     webhook.send(f"Downloaded Karaoke Song: {interpret} - {song}")
 
 
+def reload_music():
+    # ip = "192.168.2.123"
+    ip = "10.60.55.202"
+    cookie = {"keToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRlVXBkYXRlZCI6MCwiaXNBZG1pbiI6dHJ1ZSwibmFtZSI6IkZsbyIsInJvb21JZCI6MSwidXNlcklkIjoyLCJpYXQiOjE2ODA1MjU1ODV9.NZCB7SuH0NML-uf0pmC63QlQcBOU_pD1R7QIIbP_ef0"}
+    requests.get(f"http://{ip}/api/prefs/scan", cookies=cookie)
+
+
 @csrf_exempt
 def index(request):
     if request.POST:
@@ -43,12 +54,15 @@ def index(request):
             # subprocess.Popen(["youtube-dl", "-f", "best", "--output", f"{OUTPUT_DIR}/{interpret} - {song}.mp4", link])
             # command = ["youtube-dl", "-f", "best", "--output", f"{OUTPUT_DIR}/{interpret} - {song}.mp4", link]
             command = ["yt-dlp", "-f", "best", "--output", f"{OUTPUT_DIR}/{interpret} - {song}.mp4", link]
-            popen_and_call(on_exit=to_discord, popen_args=command, exit_args=[interpret, song])
+            # popen_and_call(on_exit=to_discord, popen_args=command, exit_args=[interpret, song])
+            popen_and_call(on_exit=reload_music, popen_args=command)
+
 
             template = loader.get_template('download_started.html')
             return HttpResponse(template.render({}, request))
 
-        except Exception:
+        except Exception as e:
+            print("Error:", e)
             template = loader.get_template('index.html')
             return HttpResponse(template.render({}, request))
     else:
