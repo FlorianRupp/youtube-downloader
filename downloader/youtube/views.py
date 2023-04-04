b449ddcb091b9050fd1a8e2153ca2c0b442439ec
florian.rupp95@gmail.com <florian.rupp95@gmail.com>
2023-04-04 15:44:08 +0200

    Using external config file.

--- downloader/youtube/views.py
+++ downloader/youtube/views.py
@@ -2,15 +2,18 @@
 import subprocess
 import threading
 import requests
+import socket
 
 from discord import Webhook, RequestsWebhookAdapter
 from django.http import HttpResponse
 from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
+import json
+
+with open("config.json", "r") as f:
+    config = json.load(f)
 
 # OUTPUT_DIR = "/home/ailab/karaoke/media"
-OUTPUT_DIR = r"C:\Users\Florian Rupp\Desktop\Karaoke\media"
-
 
 def popen_and_call(on_exit, popen_args, exit_args=None):
     if exit_args is None:
@@ -29,16 +32,17 @@ def popen_and_call(on_exit, popen_args, exit_args=None):
 
 
 def to_discord(interpret, song):
-    webhook = Webhook.from_url(
-        "https://ddiscord.com/api/webhooks/1069963059428343918/heT2Qy7tXdw_w-RDZNwYH5ZzBaO6r5_q2wgiOMehnQKE8qCFVf_IX21KsUPirAjp_2Vg",
+    webhook = Webhook.from_url(config["webhook"],
         adapter=RequestsWebhookAdapter())
     webhook.send(f"Downloaded Karaoke Song: {interpret} - {song}")
 
 
 def reload_music():
     # ip = "192.168.2.123"
-    ip = "10.60.55.202"
-    cookie = {"keToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRlVXBkYXRlZCI6MCwiaXNBZG1pbiI6dHJ1ZSwibmFtZSI6IkZsbyIsInJvb21JZCI6MSwidXNlcklkIjoyLCJpYXQiOjE2ODA1MjU1ODV9.NZCB7SuH0NML-uf0pmC63QlQcBOU_pD1R7QIIbP_ef0"}
+    # ip = "10.60.55.202"
+    # ip = "192.168.2.107"
+    ip = socket.gethostbyname(socket.gethostname())
+    cookie = {"keToken": config["cookie"]}
     requests.get(f"http://{ip}/api/prefs/scan", cookies=cookie)
 
 
@@ -53,7 +57,7 @@ def index(request):
 
             # subprocess.Popen(["youtube-dl", "-f", "best", "--output", f"{OUTPUT_DIR}/{interpret} - {song}.mp4", link])
             # command = ["youtube-dl", "-f", "best", "--output", f"{OUTPUT_DIR}/{interpret} - {song}.mp4", link]
-            command = ["yt-dlp", "-f", "best", "--output", f"{OUTPUT_DIR}/{interpret} - {song}.mp4", link]
+            command = ["yt-dlp", "-f", "best", "--output", f"{config['save_dir']}/{interpret} - {song}.mp4", link]
             # popen_and_call(on_exit=to_discord, popen_args=command, exit_args=[interpret, song])
             popen_and_call(on_exit=reload_music, popen_args=command)
 
